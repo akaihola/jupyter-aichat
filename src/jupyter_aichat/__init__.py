4d1bf075d7e03d7e7ae15441f7733873ffbd9028
Antti Kaihola <13725+akaihola@users.noreply.github.com>
2023-03-18 17:59:39 +0200

    Display a friendly welcome after `%load_ext`

--- src/jupyter_aichat/__init__.py
+++ src/jupyter_aichat/__init__.py
@@ -2,8 +2,9 @@
 
 __version__ = "0.0.1"
 
-from .aichat_magic import ConversationMagic
+from .aichat_magic import ConversationMagic, output, TemplateLoader
 
 
 def load_ipython_extension(ipython):
     ipython.register_magics(ConversationMagic)
+    output(TemplateLoader()["load_ipython_extension"])
---
+++ src/jupyter_aichat/data/load_ipython_extension.md
@@ -0,0 +1,1 @@
+Welcome to `jupyter-aichat`! Type `%ai` to get started.
