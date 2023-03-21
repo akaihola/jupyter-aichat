7592559f7992b32031c2318cd1e414cf815bcced
Antti Kaihola <13725+akaihola@users.noreply.github.com>
2023-03-21 21:59:09 +0200

    Support exception instances in `raises_or_matches`

--- tests/assertion.py
+++ tests/assertion.py
@@ -1,13 +1,16 @@
+import re
 from contextlib import nullcontext
-from typing import Union, Any
+from typing import Union, Any, Type
 
 import pytest
 from _pytest.python_api import RaisesContext
 
 
 def raises_or_matches(  # type: ignore[misc]
-    exception: Union[BaseException, Any]
+    exception: Union[BaseException, Type[BaseException], Any]
 ) -> Union[nullcontext[None], RaisesContext[BaseException]]:
     if isinstance(exception, type) and issubclass(exception, BaseException):
         return pytest.raises(exception)
+    if isinstance(exception, BaseException):
+        return pytest.raises(type(exception), match=f"^{re.escape(str(exception))}$")
     return nullcontext()
